9a50ea9fb868a02c7c4fee88ba0661e5393847c5
Igors Koptevs <igors.koptevs@gmail.com>
2022-08-23 16:37:27 +0300

    Navbar tuned

--- global-templates/navbar-offcanvas.php
+++ global-templates/navbar-offcanvas.php
@@ -26,12 +26,12 @@ $container = 'container';
             <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
                itemprop="url">
 	            <?php //echo get_template_directory_uri() . '/img/logo.svg'?><!--" alt="" width="30" height="24">-->
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                      class="bi bi-app-indicator" viewBox="0 0 16 16">
                     <path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z"/>
                     <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 </svg>
-                <span>
+                <span class="ms-1">
                 <?php bloginfo( 'name' ); ?>
                 </span>
             </a>
